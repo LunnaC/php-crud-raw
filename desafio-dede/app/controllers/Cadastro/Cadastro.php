9272dd470d114df303adba6266391a29462f7a1d
Lunna Camargo <lunna@pumpycode.com>
2022-12-14 15:00:25 -0300

    Organização

---
+++ desafio-dede/app/controllers/Cadastro/Cadastro.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Cadastro {
+    function __construct() {
+        
+    }
+        
+
+}
--- desafio-dede/index.php
+++
@@ -1,3 +0,0 @@
-<a href="cadastro_clientes.php"> cadastre-se </a>
-<a href="login_clientes.php"> login </a>
-<a href="dashboard_produtos.php">dashboard</a>
